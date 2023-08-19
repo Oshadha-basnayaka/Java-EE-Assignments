5444083b28dbe9bdaa96bfd8249c7ecbfe2cf89f
Oshadha Basnayaka <oshadhabasnayaka69@gmail.com>
2023-08-19 23:39:04 +0530

    doGet

--- Json-with-Ajax/src/lk/ijse/assignment/ajax/servlot/CustomerServlet.java
+++ Json-with-Ajax/src/lk/ijse/assignment/ajax/servlot/CustomerServlet.java
@@ -1,10 +1,15 @@
 package lk.ijse.assignment.ajax.servlot;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
 @WebServlet(urlPatterns = {"/page/customer"})
 public class CustomerServlet extends HttpServlet {
@@ -12,7 +17,29 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bazzafashion", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
 
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+
+                JsonObjectBuilder customerObject = Json.createObjectBuilder();
+                customerObject.add("id", id);
+                customerObject.add("name", name);
+                customerObject.add("address", address);
+                allCustomers.add(customerObject.build());
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-}
+    }
