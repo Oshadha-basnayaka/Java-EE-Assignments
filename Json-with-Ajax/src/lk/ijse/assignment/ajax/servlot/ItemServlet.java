e9f62af914ad055f45f9bb8e977b5229fda4b164
Oshadha Basnayaka <oshadhabasnayaka69@gmail.com>
2023-08-20 00:01:21 +0530

    item doGet

--- Json-with-Ajax/src/lk/ijse/assignment/ajax/servlot/ItemServlet.java
+++ Json-with-Ajax/src/lk/ijse/assignment/ajax/servlot/ItemServlet.java
@@ -1,11 +1,15 @@
 package lk.ijse.assignment.ajax.servlot;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
 @WebServlet (urlPatterns = ("/page/item"))
 public class ItemServlet   extends HttpServlet {
@@ -14,5 +18,45 @@ public class ItemServlet   extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bazzafashion", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
+
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+            while (rst.next()) {
+                String code = rst.getString(1);
+                String itemName = rst.getString(2);
+                int qty = rst.getInt(3);
+                double unitPrice = rst.getDouble(4);
+
+                JsonObjectBuilder itemObject = Json.createObjectBuilder();
+                itemObject.add("code", code);
+                itemObject.add("itemName", itemName);
+                itemObject.add("qty", qty);
+                itemObject.add("unitPrice", unitPrice);
+                allItems.add(itemObject.build());
+            }
+
+            resp.getWriter().print(allItems.build());
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
+
+
+
+
+
+
+
 }
