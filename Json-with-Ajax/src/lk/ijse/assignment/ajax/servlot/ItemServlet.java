0cd714c7de28fdd5fd1c0eedfbbcc7e51fc9e31a
Oshadha Basnayaka <oshadhabasnayaka69@gmail.com>
2023-08-20 00:18:28 +0530

    json problem

--- Json-with-Ajax/src/lk/ijse/assignment/ajax/servlot/ItemServlet.java
+++ Json-with-Ajax/src/lk/ijse/assignment/ajax/servlot/ItemServlet.java
@@ -22,7 +22,7 @@ public class ItemServlet   extends HttpServlet {
 
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bazzafashion", "root", "1234");
-            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            PreparedStatement pstm = connection.prepareStatement("select * from Item");
             ResultSet rst = pstm.executeQuery();
 
 
@@ -53,10 +53,84 @@ public class ItemServlet   extends HttpServlet {
     }
 
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String code = req.getParameter("code");
+        String itemName = req.getParameter("description");
+        String qty = req.getParameter("qty");
+        String unitPrice = req.getParameter("unitPrice");
+        String option = req.getParameter("option");
+
+
+        try {
+
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bazzafashion", "root", "1234");
 
 
 
+            switch (option){
+
+                case "add":
+                    PreparedStatement pstm = connection.prepareStatement("insert into Item values(?,?,?,?)");
+                    pstm.setObject(1, code);
+                    pstm.setObject(2, itemName);
+                    pstm.setObject(3, qty);
+                    pstm.setObject(4, unitPrice);
+                    resp.addHeader("Content-Type", "application/json");
+
+                    if (pstm.executeUpdate() > 0) {
+                        JsonObjectBuilder response = Json.createObjectBuilder();
+                        response.add("state", "Ok");
+                        response.add("message", "Successfully Added.!");
+                        response.add("data", "");
+                        resp.getWriter().print(response.build());
+                    }
+                    break;
 
 
+                case "delete":
+                    PreparedStatement pstm2 = connection.prepareStatement("delete from Item where code=?");
+                    pstm2.setObject(1, code);
+                    resp.addHeader("Content-Type", "application/json");
 
+                    if (pstm2.executeUpdate() > 0) {
+                        JsonObjectBuilder response = Json.createObjectBuilder();
+                        response.add("state", "Ok");
+                        response.add("message", "Item Deleted..!");
+                        response.add("data", "");
+                        resp.getWriter().print(response.build());
+                    }
+                    break;
+
+                case "update":
+                    PreparedStatement pstm3 = connection.prepareStatement("update Item set itemName=?,qty=?,unitPrice=? where code=?");
+                    pstm3.setObject(1, itemName);
+                    pstm3.setObject(2, qty);
+                    pstm3.setObject(3, unitPrice);
+                    pstm3.setObject(4, code);
+                    resp.addHeader("Content-Type", "application/json");
+
+                    if (pstm3.executeUpdate() > 0) {
+                        JsonObjectBuilder response = Json.createObjectBuilder();
+                        response.add("state", "Ok");
+                        response.add("message", "Item Updated..!");
+                        response.add("data", "");
+                        resp.getWriter().print(response.build());
+                    }
+                    break;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("state", "Error");
+            response.add("message", e.getMessage());
+            response.add("data", "");
+            resp.setStatus(400);
+            resp.getWriter().print(response.build());
+        }
+    }
 }
